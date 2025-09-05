6f1ce2e63acdae837f4e3ebbe290c44358d65b7c
Kaikes0825 <knittelkai0@gmail.com>
2025-09-05 10:09:10 +0000

    Uebung 1 + 2 + 3

--- com/cc/java/Uebung03.java
+++ com/cc/java/Uebung03.java
@@ -1,12 +1,49 @@
 package com.cc.java;
 
-public class App 
+import java.util.Scanner;
+
+public class Uebung03 
 {
-
     public static void main(String[] args) {
-        System.out.println("Uebung 3");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bitte geben Sie die erste Zahl ein: ");
+        double zahl1 = scanner.nextDouble();
+
+        System.out.print("Bitte geben Sie die zweite Zahl ein: ");
+        double zahl2 = scanner.nextDouble();
+
+        System.out.print("Welche Rechenoperation möchten Sie durchführen? (Addition, Subtraktion, Multiplikation, Division): ");
+        String operation = scanner.next().toLowerCase();
+
+        double ergebnis;
+        switch (operation) {
+            case "addition":
+                ergebnis = zahl1 + zahl2;
+                break;
+            case "subtraktion":
+                ergebnis = zahl1 - zahl2;
+                break;
+            case "multiplikation":
+                ergebnis = zahl1 * zahl2;
+                break;
+            case "division":
+                if (zahl2 != 0) {
+                    ergebnis = zahl1 / zahl2;
+                } else {
+                    System.out.println("Division durch Null ist nicht erlaubt.");
+                    scanner.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Unbekannte Operation.");
+                scanner.close();
+                return;
+        }
+
+        System.out.println("Das Ergebnis lautet: " + ergebnis);
+
+        scanner.close();
     }
-    
 }
-
-
