3b19107aac549b5f1b5242a3e27df7bcb8b65666
Kaikes0825 <knittelkai0@gmail.com>
2025-09-05 10:11:40 +0000

    Uebung 1 + 2 + 3 + 4

--- com/cc/java/Uebung04.java
+++ com/cc/java/Uebung04.java
@@ -1,12 +1,30 @@
 package com.cc.java;
 
-public class App 
+import java.util.Scanner;
+
+public class Uebung04 
 {
-
     public static void main(String[] args) {
-        System.out.println("Uebung 4");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Bitte geben Sie eine ganze Zahl von 0 bis 15 ein: ");
+        int zahl = scanner.nextInt();
+
+        if (zahl < 0 || zahl > 15) {
+            System.out.println("Ungültige Eingabe. Bitte geben Sie eine Zahl von 0 bis 15 ein.");
+            scanner.close();
+            return;
+        }
+
+        String dualzahl = "";
+        int temp = zahl;
+        do {
+            dualzahl = (temp % 2) + dualzahl;
+            temp = temp / 2;
+        } while (temp > 0);
+
+        System.out.println("Die Dualzahl lautet: " + dualzahl);
+
+        scanner.close();
     }
-    
 }
-
-
