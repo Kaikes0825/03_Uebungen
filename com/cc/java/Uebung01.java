f8fa89e9c23cb502fe9481716b6049eff966f4ad
Kaikes0825 <knittelkai0@gmail.com>
2025-09-05 09:51:07 +0000

    Uebung 1.1

--- com/cc/java/Uebung01.java
+++ com/cc/java/Uebung01.java
@@ -1,12 +1,20 @@
 package com.cc.java;
 
-public class App 
+import java.util.Scanner;
+
+public class Uebung01 
 {
-
     public static void main(String[] args) {
-        System.out.println("Uebung 1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bitte geben Sie eine ganze Zahl ein: ");
+        int zahl = scanner.nextInt();
+
+        if (zahl % 2 == 0) {
+            System.out.println("Diese Zahl ist gerade.");
+        } else {
+            System.out.println("Diese Zahl ist ungerade.");
+        }
+
+        scanner.close();
     }
-    
 }
-
-
